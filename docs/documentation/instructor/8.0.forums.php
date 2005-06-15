7dd2a0eacede691e9076270b0f936a8cdf1f3c6a
greg gay <ggay@ocad.ca>
2005-06-15 16:04:02 +0000

    tweaked

--- docs/documentation/instructor/8.0.forums.php
+++ docs/documentation/instructor/8.0.forums.php
@@ -1,4 +1,4 @@
-<?php require('../common/body_header.inc.php'); ?>
+<?php require('../common/body_header.inc.php'); ?> 
 
 <h2>8. Forums</h2>
 	<p>A Forum is a student tool that allows all course members to communicate in a structured manner through messages. Being a <a href="./14.0.student_tools.php">Student Tool</a>, the forums can be enabled, disabled, and linked from the main navigation, the course home page, or displayed as a menu module as choosen in the Student Tools preferences.</p>
