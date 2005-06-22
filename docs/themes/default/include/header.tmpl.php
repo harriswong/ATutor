f4fa786be37ad06f952d68882fb7743db798361a
heidi valles <hvalles@ocad.ca>
2005-06-22 20:49:56 +0000

    alt txt to prev/next links

--- docs/themes/default/include/header.tmpl.php
+++ docs/themes/default/include/header.tmpl.php
@@ -316,14 +316,13 @@ function toggleToc(objId) {
 
 	<div style="float:right;padding-top:7px;">
 		<?php if ($this->sequence_links['resume']): ?>
-				<a style="color:white;" href="<?php echo $this->sequence_links['resume']['url']; ?>" accesskey="."><img src="<?php echo $this->base_path; ?>images/resume.gif" border="0" title="<?php echo _AT('resume').': '.$this->sequence_links['resume']['title']; ?>" alt="<?php echo $this->sequence_links['resume']['title']; ?>"></a>
-
+				<a style="color:white;" href="<?php echo $this->sequence_links['resume']['url']; ?>" accesskey="."><img src="<?php echo $this->base_path; ?>images/resume.gif" border="0" title="<?php echo _AT('resume').': '.$this->sequence_links['resume']['title']; ?>" alt="<?php echo $this->sequence_links['resume']['title']; ?>" /></a>
 		<?php else:
 			if ($this->sequence_links['previous']): ?>
-				<a href="<?php echo $this->sequence_links['previous']['url']; ?>" title="<?php echo _AT('previous_topic').': '. $this->sequence_links['previous']['title']; ?>" accesskey=","><img src="<?php echo $this->base_path; ?>images/previous.gif" border="0"></a>
+				<a href="<?php echo $this->sequence_links['previous']['url']; ?>" title="<?php echo _AT('previous_topic').': '. $this->sequence_links['previous']['title']; ?>" accesskey=","><img src="<?php echo $this->base_path; ?>images/previous.gif" border="0" alt="<?php echo _AT('previous_topic').': '. $this->sequence_links['previous']['title']; ?>" /></a>
 			<?php endif;
 			if ($this->sequence_links['next']): ?>
-				<a href="<?php echo $this->sequence_links['next']['url']; ?>" title="<?php echo _AT('next_topic').': '.$this->sequence_links['next']['title']; ?>" accesskey="."><img src="<?php echo $this->base_path; ?>images/next.gif" border="0"></a>
+				<a href="<?php echo $this->sequence_links['next']['url']; ?>" title="<?php echo _AT('next_topic').': '.$this->sequence_links['next']['title']; ?>" accesskey="."><img src="<?php echo $this->base_path; ?>images/next.gif" border="0" alt="<?php echo _AT('next_topic').': '.$this->sequence_links['next']['title']; ?>" /></a>
 			<?php endif; ?>
 		<?php endif; ?>
 		&nbsp;
