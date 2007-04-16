5516a2dc888e2eebfe46aa5555ec56eda88f50a4
joel kronenberg <joel.kronenberg@utoronto.ca>
2007-04-16 18:43:03 +0000

    http://www.atutor.ca/atutor/mantis/view.php?id=3031

--- docs/forum/view.php
+++ docs/forum/view.php
@@ -29,6 +29,7 @@ if (!valid_forum_user($fid)) {
 	require(AT_INCLUDE_PATH.'footer.inc.php');
 	exit;
 }
+$forum_info = get_forum($fid);
 $_pages['forum/index.php?fid='.$fid]['title']    = get_forum_name($fid);
 $_pages['forum/index.php?fid='.$fid]['parent']   = 'forum/list.php';
 $_pages['forum/index.php?fid='.$fid]['children'] = array('forum/new_thread.php?fid='.$fid);
